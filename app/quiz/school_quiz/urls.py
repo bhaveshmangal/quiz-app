d79cdeb601c4d086e7161dc06518a8e270ced3cc
Bhavesh Mangal <bm6379@gmail.com>
2022-10-03 19:10:21 +0530

    student-score-model

--- app/quiz/school_quiz/urls.py
+++ app/quiz/school_quiz/urls.py
@@ -1,10 +1,11 @@
 from rest_framework.routers import DefaultRouter
-from .views import StudentClassViewSet, StudentViewSet, ClassTestViewSet
+from .views import StudentClassViewSet, StudentViewSet, ClassTestViewSet, StudentScoreViewSet
 
 router = DefaultRouter()
 
 router.register(r'student', StudentViewSet, basename='student')
 router.register(r'student_class', StudentClassViewSet, basename='student_class')
 router.register(r'test', ClassTestViewSet, basename='test')
+router.register(r'score', StudentScoreViewSet, basename='score')
 
 urlpatterns = router.urls
